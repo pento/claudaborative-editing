f3b0a6b2330d5b7f10148d2aaf7b5030e9ea7f89
Gary Pendergast <gary@pento.net>
2026-03-30 09:02:19 +1100

    dev: Automatically run composer install when starting the test environment.

--- bin/after-start.sh
+++ bin/after-start.sh
@@ -14,6 +14,7 @@ if [ "$1" = "--build-gutenberg" ]; then
 fi
 
 if [ "$1" = "--test-env" ]; then
+	npx wp-env --config .wp-env.test.json run cli --env-cwd=wp-content/plugins/claudaborative-editing composer install --no-interaction --prefer-dist
 	npx wp-env --config .wp-env.test.json run cli wp plugin activate gutenberg claudaborative-editing
 	npx wp-env --config .wp-env.test.json run cli wp option update wp_collaboration_enabled 1
 else
