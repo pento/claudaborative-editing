997868f9ca7c88c4add195c999f4ba4ff71f4693
Gary Pendergast <gary@pento.net>
2026-03-29 19:56:29 +1100

    fix: Use the correct wp-env config when testing.

--- bin/after-start.sh
+++ bin/after-start.sh
@@ -13,5 +13,10 @@ if [ "$1" = "--build-gutenberg" ]; then
 	cd - > /dev/null
 fi
 
-npx wp-env run cli wp plugin activate gutenberg claudaborative-editing
-npx wp-env run cli wp option update wp_collaboration_enabled 1
+if [ "$1" = "--test-env" ]; then
+	npx wp-env --config .wp-env.test.json run cli wp plugin activate gutenberg claudaborative-editing
+	npx wp-env --config .wp-env.test.json run cli wp option update wp_collaboration_enabled 1
+else
+	npx wp-env run cli wp plugin activate gutenberg claudaborative-editing
+	npx wp-env run cli wp option update wp_collaboration_enabled 1
+fi
