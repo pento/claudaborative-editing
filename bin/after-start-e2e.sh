c61872b94650da75d5d0366972f8aad9ecdf8f7c
Gary Pendergast <gary@pento.net>
2026-04-03 14:38:08 +1100

    fix: plugin build has to happen after composer install.

--- bin/after-start-e2e.sh
+++ bin/after-start-e2e.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
-npm run build
-npm --prefix wordpress-plugin run build
+npx wp-env --config .wp-env.test.json run cli --env-cwd=wp-content/plugins/claudaborative-editing composer install --no-interaction --prefer-dist
+
+npm run build:all
 
 npx wp-env --config .wp-env.test.json run cli wp plugin activate gutenberg claudaborative-editing
 npx wp-env --config .wp-env.test.json run cli wp option update wp_collaboration_enabled 1
